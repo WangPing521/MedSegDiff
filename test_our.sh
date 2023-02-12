e056900691eaaf8194102d5bde61d6d96111346b
WangPing521 <wp_scdedu@163.com>
2023-02-12 17:04:56 -0500

    bs

--- test_our.sh
+++ test_our.sh
@@ -10,7 +10,7 @@ time=6
 account=def-chdesa
 declare -a StringArray=(
 
-"python segmentation_train.py --data_name MMWHS --image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16 --diffusion_steps 200 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False --lr 1e-4 --batch_size 8"
+"python segmentation_train.py --data_name MMWHS --image_size 256 --num_channels 128 --class_cond False --num_res_blocks 2 --num_heads 1 --learn_sigma True --use_scale_shift_norm False --attention_resolutions 16 --diffusion_steps 200 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False --lr 1e-4 --batch_size 4"
 
 )
 
